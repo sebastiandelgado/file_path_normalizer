84fa648eb14f9a81d8a82e756398890f67e3a39c
sebastiandelgado <sdalcega@gmail.com>
2013-03-04 16:28:01 -0800

    Create file_path_norm.rb

---
+++ file_path_norm.rb
@@ -0,0 +1,26 @@
+
+def normalize(filePath)
+
+  fp = filePath
+
+	# remove relevant '.'
+	while fp =~ /\/\.\/|\A\.\/|\/\.\z/
+		fp = fp.gsub(/\/\.\/|\A\.\/|\/\.\z/, "/") 
+	end
+
+	# remove relevant '..' and their parent directories if any
+	while fp =~ /\A\.\.\/|\/\.\.\/|\/\.\.\z/ 
+		#  MATCH: ('/' or beginning of string) + (any text uninterrupted by '/' that is not only '..') 
+		#  + ('/..') + ('/' or end of string)
+		fp = fp.gsub(/(\/|\A)([^\/]*|[^\.\/].[^\.\/].)\/\.\.(\/|\z)/, "/")
+		# remove '..' referring to inexistent parent directory
+		fp = fp.gsub(/\A\/?\.\.\//, "")
+	end
+
+	# Remove '/' at string boundaries
+	fp = fp.gsub(/\A\/|\/\z/, "")
+
+	return fp
+end
+
+$stdout.print normalize(STDIN.gets)
